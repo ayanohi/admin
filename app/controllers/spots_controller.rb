2ae95e45bc15ac2f824387fd2d3aa90126f404f6
ayanohi <pllayam@yahoo.co.jp>
2021-02-14 20:42:21 +0900

    feat: スポットのcreateメソッド定義

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -3,4 +3,23 @@ class SpotsController < ApplicationController
   def new
     @spot = Spot.new
   end
+
+  def create
+    @spot = Spot.new(spot_params)
+    if @spot.save
+      redirect_to spots_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def spot_params
+    params.require(:spot).permit(
+      :name,
+      :description,
+      :postal_code,
+      :address
+    )
+  end
 end
